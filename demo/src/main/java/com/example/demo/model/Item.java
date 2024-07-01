06e15eb1699304e1321d10d21bc659b39da4b3fb
Sanyam Kumar <sanyam.kumar@olx.in>
2024-07-01 23:59:05 +0530

    set up the project, test simple api, make db connection

--- demo/src/main/java/com/example/demo/model/Item.java
+++ demo/src/main/java/com/example/demo/model/Item.java
@@ -1,5 +1,7 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.*;
 import lombok.Data;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,8 +19,43 @@ public class Item {
     @Column
     private long createdBy;
 
-    private
+    @Column
+    private String Type;
 
+    @Column
+    private long costPrice;
 
+    @Column
+    private long sellingPrice;
+
+    @Column
+    private String creationDate;
+
+    @Column
+    private String lastUpdatedDate;
+
+    @Column String attribute;
+
+    public void setAttribute(JsonNode attribute) {
+
+        try{
+            ObjectMapper objectMapper = new ObjectMapper();
+            this.attribute = objectMapper.writeValueAsString(attribute);
+        }
+        catch(Exception e){
+            throw new RuntimeException("unable to serialize JSON");
+        }
+    }
+
+    public JsonNode getAttribute() {
+
+        try{
+            ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.readTree(this.attribute);
+        }
+        catch (Exception e){
+            throw new RuntimeException("unable to deserialize JSON");
+        }
+    }
 
 }
